070daaa5de7bce33c6e533a3d9c73c1d83cf65ee
= <cocoecco@yahoo.com>
2018-11-30 15:26:20 +0200

    removed pods folder

--- Pods/Target Support Files/Pods-RestaurantsExplorer/Pods-RestaurantsExplorer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RestaurantsExplorer : NSObject
-@end
-@implementation PodsDummy_Pods_RestaurantsExplorer
-@end
--- Pods/Target Support Files/Pods-RestaurantsExplorerTests/Pods-RestaurantsExplorerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RestaurantsExplorerTests : NSObject
-@end
-@implementation PodsDummy_Pods_RestaurantsExplorerTests
-@end
