615db938ac8feaf6a0d1a7b62cc39bad344e2d1a
= <cocoecco@yahoo.com>
2018-12-02 10:08:20 +0200

    added two unit tests

--- RestaurantsExplorerTests/RestaurantsExplorerTests.swift
+++ RestaurantsExplorerTests/RestaurantsExplorerTests.swift
@@ -11,20 +11,50 @@ import XCTest
 
 class RestaurantsExplorerTests: XCTestCase {
 
+    var requestDispatcher: RequestDispatcher!
+    var searchLocationsService: SearchLocationsService!
+    
     override func setUp() {
+        self.requestDispatcher = RequestDispatcher()
+        self.searchLocationsService = SearchLocationsService(requestDispatcher: self.requestDispatcher)
     }
 
-    override func tearDown() {
+    override func tearDown() {}
+
+    func testApp() {
+        self.citiesFoundTest()
+        self.citiesNotFoundTest()
     }
-
-    func testExample() {
-
-    }
-
-    func testPerformanceExample() {
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func citiesFoundTest() {
+        let existingCitiesTerm = "te"
+        let promise = expectation(description: "Should return with results")
+        
+        self.searchLocationsService.searchLocation(term: existingCitiesTerm)
+            .then { results in
+                let citiesCount = results.cities.count
+                XCTAssert((citiesCount > 0), "Cities count should be larger then 0")
+                promise.fulfill()
+            }.catch { error in
+                XCTFail("Status code: \(error)")
         }
+        
+        waitForExpectations(timeout: 5, handler: nil)
     }
-
+    
+    func citiesNotFoundTest() {
+        let nonExistingCitiesTerm = "tejkjkfdsf"
+        let promise = expectation(description: "Should return with 0 results")
+        
+        self.searchLocationsService.searchLocation(term: nonExistingCitiesTerm)
+            .then { results in
+                let citiesCount = results.cities.count
+                XCTAssert((citiesCount == 0), "Cities count should be 0")
+                promise.fulfill()
+            }.catch { error in
+                XCTFail("Status code: \(error)")
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
 }
